faaeca72e828637892be3ab70b3c3894e14a8186
Mike Reich <mjreich@gmail.com>
2015-07-16 15:22:53 -0700

    Added src folder and compile to lib

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 * @Author: mike
 * @Date:   2015-05-18 16:56:47
 * @Last Modified 2015-07-16
-* @Last Modified time: 2015-07-16 09:13:18
+* @Last Modified time: 2015-07-16 15:05:05
 */
 
-require("babel/register")()
+require("babel").transform("code", { optional: ["runtime"] });
 
 module.exports = require('./lib/Application')
