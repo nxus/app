46d54c737ab654ba61334a1944317dde716376ce
Mike Reich <mjreich@gmail.com>
2015-07-16 15:41:21 -0700

    Actually removed the unused babel ref and incremented version

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 * @Author: mike
 * @Date:   2015-05-18 16:56:47
 * @Last Modified 2015-07-16
-* @Last Modified time: 2015-07-16 15:05:05
+* @Last Modified time: 2015-07-16 15:40:23
 */
-
-require("babel").transform("code", { optional: ["runtime"] });
 
 module.exports = require('./lib/Application')
