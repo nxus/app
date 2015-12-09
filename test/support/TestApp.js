1ec5947971e28f4295c213ed092a54d556f7b199
Luke Opperman <luke@opperman.net>
2015-12-09 09:02:32 -0500

    Add module on to TestApp

--- test/support/TestApp.js
+++ test/support/TestApp.js
@@ -15,16 +15,18 @@ class TestApp extends Dispatcher {
     this.on = sinon.spy(this.on);
     this.once = sinon.spy(this.once);
 
-    this.await = sinon.spy();
-    
     this._gather = sinon.createStubInstance(Promise);
     this._send_with = sinon.createStubInstance(Promise);
+    this._get_on = sinon.spy();
+    this._get_once = sinon.spy();
     this._send = {
       with: sinon.stub().returns(this._send_with)
     }
     this._get = {
       gather: sinon.stub().returns(this._gather),
-      send: sinon.stub().returns(this._send)
+      send: sinon.stub().returns(this._send),
+      on: sinon.stub().returns(this._get_on)
+      once: sinon.stub().returns(this._get_once)
     };
     this.get = sinon.stub().returns(this._get);
 
