2a59b625d63b9e96bc6082ba1306b44d63c62735
Mike Reich <mjreich@gmail.com>
2016-09-13 12:51:07 -0700

    Reverted changed to NxusModule adding disabled

--- src/NxusModule.js
+++ src/NxusModule.js
@@ -34,12 +34,6 @@ class NxusModule {
 
     this._dirName = __dirName(this.constructor.name)
 
-    if(application.config[this.__name] && typeof application.config[this.__name].disabled != 'undefined' && application.config[this.__name].disabled) {
-      this.disabled = true
-      this.log.info(this.__name, 'disabled, not registering with Application')
-      return
-    }
-
     this.__proxy = application.get(this.__name)
     this.__proxy.use(this)
   }
